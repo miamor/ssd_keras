ba84619e600513a9910d8729dbf93e3dadfdf19b
tunguyen <tunguyenhsgs@gmail.com>
2019-03-27 09:41:22 +0700

    27.03.19

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ python get_data_from_XML.py
 Modify appropriate content 
 ## Train
 ```
-python ssd_training_japanese.py
+python ssd_mobilenet.py train -o ./output/training_logs -w ./weights/mobilenet_ssd.hdf5
 ```
 ## Infer
 ```
-python ssd_infer.py -i ./test_images -o ./output/results -w ./output/training_logs/run1/checkpoint-276-2.7208.hdf5 -t 0.6
+python ssd_mobilenet.py infer -i ./test_images -o ./output/results -w ./output/training_logs/run1/checkpoint-276-2.7208.hdf5 -t 0.6
 ```
--- ssd_mobilenet.py
+++ ssd_mobilenet.py
@@ -1,3 +1,4 @@
+import keras
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
 from keras.preprocessing import image
 from keras.applications.imagenet_utils import preprocess_input
@@ -12,6 +13,7 @@ import argparse
 from SSD300.ssd_v2 import SSD300v2
 from SSD300.ssd_training import MultiboxLoss
 from SSD300.ssd_utils import BBoxUtility
+#from SSD300.ssd300MobileNetV2Lite import SSD
 from SSD300.ssd300MobileNet import SSD
 
 from get_data_from_XML import XML_preprocessor
@@ -21,36 +23,80 @@ from generator import Generator
 voc_classes = ['Aeroplane', 'Bicycle', 'Bird', 'Boat', 'Bottle',
                'Bus', 'Car', 'Cat', 'Chair', 'Cow', 'Diningtable',
                'Dog', 'Horse','Motorbike', 'Person', 'Pottedplant',
-               'Sheep', 'Sofa', 'Train', 'Tvmonitor']
-voc_classes = ['Aeroplane', 'Bicycle', 'Bird', 'Boat', 'Bottle',
-               'Bus', 'Car', 'Cat', 'Chair', 'Cow', 'Diningtable',
-               'Dog', 'Horse','Motorbike', 'Person', 'Pottedplant',
                'Sheep', 'Sofa', 'Train', 'Tvmonitor',
                'p0', 'p1', 'p2']
 '''
 voc_classes = ['p0', 'p1', 'p2']
 NUM_CLASSES = len(voc_classes) + 1
+#NUM_CLASSES = 4
+#batch_size = 128
+#batch_size = 32
+batch_size = 16
 input_shape = (300, 300, 3)
 
+#priorFile = './priorFiles/prior_boxes_ssd300MobileNetV2.pkl'
+priorFile = './priorFiles/prior_boxes_ssd300MobileNet.pkl'
+priors = pickle.load(open(priorFile, 'rb'))
+bbox_util = BBoxUtility(NUM_CLASSES, priors)
 
-def infer(args):
-    thresh = args.thresh
-    weights = args.weight
+
+def train(args):
+    #weights = './weights/mobilenet_ssd.hdf5'
+    #weights = './output/training_logs/run1/checkpoint-21-5.6296.hdf5'
+    #weights = './output/training_logs/run1/checkpoint-37-3.4409.hdf5'
 
     #model = SSD300v2(input_shape, num_classes=NUM_CLASSES)
-    model = SSD(input_shape, num_classes=NUM_CLASSES, weights=weights)
+    model = SSD(input_shape, num_classes=NUM_CLASSES, weights=args.weight)
+    model.summary()
+    model.load_weights(args.weight, by_name=False)
+    print('Loaded weights to model.')
 
     loss = MultiboxLoss(NUM_CLASSES, neg_pos_ratio=2.0).compute_loss
     model.compile(optimizer='Adadelta', loss=loss)
-    # model.summary()
+    #base_lr = 3e-4
+    #optim = keras.optimizers.Adam(lr=base_lr)
+    #model.compile(optimizer=optim, loss=loss)
 
+    #data_parser = XML_preprocessor(data_path='/media/tunguyen/Others/Dataset/JapaneseCard/annotations/', num_classes=NUM_CLASSES-1)
+    #keys = list(data_parser.data.keys())
+
+    data_parser = pickle.load(open('japanese_data.pkl', 'rb'))
+    keys = sorted(data_parser.keys())
+
+    train_num = int(0.7 * len(keys))
+    train_keys = keys[:train_num]
+    val_keys = keys[train_num:]
+
+    gen = Generator(gt=data_parser, bbox_util=bbox_util,
+                    batch_size=batch_size, path_prefix='',
+                    train_keys=train_keys, val_keys=val_keys, image_size=(300, 300))
+    RUN = RUN + 1 if 'RUN' in locals() else 1
+
+    LOG_DIR = './{}/run{}'.format(args.output_dir, RUN)
+    LOG_FILE_PATH = LOG_DIR + '/checkpoint-{epoch:02d}-{val_loss:.4f}.hdf5'
+
+    EPOCHS = 8000
+
+    tensorboard = TensorBoard(log_dir=LOG_DIR, write_images=True)
+    checkpoint = ModelCheckpoint(
+        filepath=LOG_FILE_PATH, monitor='val_loss', verbose=1, save_best_only=True)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1)
+
+    history = model.fit_generator(generator=gen.generate(True), steps_per_epoch=(gen.train_batches//gen.batch_size),
+                                  validation_data=gen.generate(False), validation_steps=(gen.train_batches//gen.batch_size),
+                                  epochs=EPOCHS, verbose=1, callbacks=[tensorboard, checkpoint])
+
+
+def infer(args):
+    #model = SSD300v2(input_shape, num_classes=NUM_CLASSES)
+    model = SSD(input_shape, num_classes=NUM_CLASSES, weights=args.weight)
+
+    loss = MultiboxLoss(NUM_CLASSES, neg_pos_ratio=2.0).compute_loss
+    model.compile(optimizer='Adadelta', loss=loss)
+    model.summary()
     #model.load_weights('./weights/MobileNetSSD300weights_voc_2007.hdf5', by_name=True)
-    model.load_weights(weights, by_name=False)
+    model.load_weights(args.weight, by_name=False)
 
-    #priors = pickle.load(open('./priorFiles/prior_boxes_ssd300.pkl', 'rb'))
-    priors = pickle.load(
-        open('./priorFiles/prior_boxes_ssd300MobileNet.pkl', 'rb'))
-    bbox_util = BBoxUtility(NUM_CLASSES, priors)
 
     inputs = []
     images = []
@@ -68,6 +114,7 @@ def infer(args):
     preds = model.predict(inputs, batch_size=1, verbose=1)
     results = bbox_util.detection_out(preds)
 
+
     colors = [None] * NUM_CLASSES
     for j, img in enumerate(images):
         # Parse the outputs.
@@ -79,7 +126,7 @@ def infer(args):
         det_ymax = results[j][:, 5]
 
         # Get detections with confidence higher than 0.6.
-        top_indices = [i for i, conf in enumerate(det_conf) if conf >= thresh]
+        top_indices = [i for i, conf in enumerate(det_conf) if conf >= args.thresh]
 
         top_conf = det_conf[top_indices]
         top_label_indices = det_label[top_indices].tolist()
@@ -104,9 +151,9 @@ def infer(args):
                     (np.random.choice(range(256), size=3)).reshape(1, -1)[0])
 
             #cv2.rectangle(img, (xmin, ymin), (xmax, ymax), colors[label], 1)
-            cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), 2)
+            cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (255, 255, 0), 2)
             #cv2.putText(img, display_txt, (xmax, ymin), cv2.FONT_HERSHEY_SIMPLEX, 1, colors[label], 2)
-            cv2.putText(img, display_txt, (xmin, ymax), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.putText(img, display_txt, (xmin+10, ymax-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 0), 2)
 
         if not os.path.isdir(args.output_dir):
             os.makedirs(args.output_dir)
@@ -117,13 +164,24 @@ def infer(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input_dir', action='store',
-                        type=str, default='./test_images')
-    parser.add_argument('-w', '--weight', action='store',
+
+    subparsers = parser.add_subparsers(dest='mode', help="Mode")
+    trainParser = subparsers.add_parser('train')
+    inferParser = subparsers.add_parser('infer')
+
+    trainParser.add_argument('--ldaDim', type=int, default=-1)
+    trainParser.add_argument('-w', '--weight', action='store',
                         type=str, default='./weights/mobilenet_ssd.hdf5')
-    parser.add_argument('-t', '--thresh', action='store',
+    trainParser.add_argument('-o', '--output_dir', action='store',
+                        type=str, default='./output/training_logs')
+
+    inferParser.add_argument('-i', '--input_dir', action='store',
+                        type=str, default='./test_images')
+    inferParser.add_argument('-w', '--weight', action='store',
+                        type=str, default='./weights/mobilenet_ssd.hdf5')
+    inferParser.add_argument('-t', '--thresh', action='store',
                         type=float, default='0.6')
-    parser.add_argument('-o', '--output_dir', action='store',
+    inferParser.add_argument('-o', '--output_dir', action='store',
                         type=str, default='./output/results')
 
     args = parser.parse_args()
